2f7d301a83668575943aa03018e16ab52eae9c94
WangJie <i@i8e.net>
2018-02-22 04:57:16 +0800

    feat: add resource handler simply

--- src/main/java/com/nullteam/ragpicker/config/WebConfig.java
+++ src/main/java/com/nullteam/ragpicker/config/WebConfig.java
@@ -1,18 +1,35 @@
 package com.nullteam.ragpicker.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
+    private final Config config;
+
+    @Autowired
+    public WebConfig(Config config) {
+        this.config = config;
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "cache-control", "x-requested-with");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/web/**")
+                .addResourceLocations("classpath:/public/");
+
+        super.addResourceHandlers(registry);
     }
 }
